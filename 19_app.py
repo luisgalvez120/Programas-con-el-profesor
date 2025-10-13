a7d38640fedcc16f38126e97f26647456c8f9a77
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-13 00:56:46 +0000

    Cardiologia

--- 19_app.py
+++ 19_app.py
@@ -3,3 +3,17 @@
 print("Precio S/.35")
 print("Para solo público del mercado 3 de Febreo")
 print("Vamos a descartar enfermedades cardiácas")
+
+# Calcular el 18% de IGV a partir de un monto de pago
+
+# Inicio: Definir o crear las variables
+monto_pago = 25
+igv = 0.18
+
+# Proceso: Realizar las operaciones de cálculo 
+monto_a_pagar = monto_pago + (monto_pago * igv)
+
+# Salida: Visualización de resultados
+print(f"El monto es: {monto_pago}")
+print(f"El IGV es: {igv}")
+print(f"El monto a pagar es: {monto_a_pagar}")
