f5c0e3a1bfa2a90228c045ad88195b0c083e6ae8
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-13 00:19:15 +0000

    Actualizamos ejercicio

---
+++ 10_app.py
@@ -0,0 +1,15 @@
+# Estructura Condicional If (si)
+
+# Sintaxis básica
+# if condición:
+#   instrucciones a ejecutarse si la condición es verdadera   
+
+# Ejemplo
+# Definir una variable cantidad
+cantidad = 8
+# cantidad = 3
+
+# Hacer comparación con if
+# La variable cantidad es mayor o igual a 5
+if cantidad >=5:
+    print("El valor de la variable cantidad es mayor o igual a 5")
