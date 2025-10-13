1d377d615e2511429f2ad03bdb5c5fd5d809cecf
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-13 00:28:34 +0000

    Ejemplo con lista de datos de cadena y números

---
+++ 17_app.py
@@ -0,0 +1,13 @@
+# Estructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de notas de exámenes
+notas = ["16", "10", 5, 18, 20]
+
+for nota in notas:
+    print(nota)
