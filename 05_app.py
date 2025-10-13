d6e0c55b650248ba5ca55d6b88b3a65597774cef
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-13 00:07:57 +0000

    Ejercicios de notas

---
+++ 05_app.py
@@ -0,0 +1,12 @@
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+# Inicio: Definir variables de las notas
+nota_t1 = 16
+nota_t2 = 10
+nota_ef = 13
+
+# Proceso: Definir el proceso de cálculo del promedio de notas
+# T1 (20%) + T2 (35%) + EF (45%)
+
+
+# Salida: Mostrar el resultado del promedio
