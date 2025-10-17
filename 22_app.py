055d665f3abbc6e209de3e9959e73a211ab7c949
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-17 03:04:46 +0000

    Ejercicio con funciones

---
+++ 22_app.py
@@ -0,0 +1,8 @@
+# Ejercicio con funciones
+def saludar(nombre):
+    print(f"Vamos con todo, {nombre} bienvenido al equipo");
+
+# Probando nuestra funsion
+saludar("Luis Galvez");
+saludar("Bella Luz");
+saludar("Maria Reyes");
