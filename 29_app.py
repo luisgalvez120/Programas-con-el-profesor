890d1ba2d5ba33a84a91f311fe9822aaeda2d799
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-24 03:39:51 +0000

    nota con nombre

---
+++ 29_app.py
@@ -0,0 +1,12 @@
+# Lista
+
+# Elementos de cadena y números
+elementos = [20, "Claudia", 12.50, "Jesús Canales", True]
+
+# Imprime lista de elementos
+print(elementos)
+
+# Imprimir los 3 primeros elementos
+print(elementos[0:3])
+
+print(elementos[0:2])
