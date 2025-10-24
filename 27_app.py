fcf271992ec1420b3d587a4cca8f7ce054622c0c
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-24 03:24:30 +0000

    Ejercicio con posicion de lista

---
+++ 27_app.py
@@ -0,0 +1,13 @@
+# Lista
+
+# Creamos nustra primera lista
+diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+
+# Imprimir la lista de elementos
+print(diasSemana)
+
+# Imprimir el tercer elemento
+print(diasSemana[2]) # posicion de indexacion
+
+# Imprimir el quinto elemento
+print(diasSemana[4]) 
