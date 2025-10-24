19a4d20fb7786051876f0c4d6b2097908467f708
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-24 03:49:59 +0000

    Obteniendo cantidad de eleentos en una lista

---
+++ 30_app.py
@@ -0,0 +1,16 @@
+# Lista
+
+# Listas anidadas
+estudiantes = [["Fabiola", 21], ["Pablo", 15], ["Angela", 20]]
+
+# Imprimir elementos de lista
+print(estudiantes)
+
+# Imprimiendo elemento de posición 0
+print(estudiantes[0])
+
+# Imprimiendo los 2 últimos elementos de la lista
+print(estudiantes[1:3])
+
+# Obtener la cantidad de elementos que tiene una lista
+print(len(estudiantes))
