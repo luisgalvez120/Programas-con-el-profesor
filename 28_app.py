736656c6f3abbcd9fe0185ac52ab44ad9d316e09
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-24 03:30:21 +0000

    notas de un alumno

---
+++ 28_app.py
@@ -0,0 +1,13 @@
+# Lista
+
+# Listado de 6 notasde un estudiante
+notas = [10, 17, 20, 15, 17]
+
+# Imprimir listado de notas
+print(notas)
+
+# Imprimir la ultima nora
+print(notas[-1])
+
+# Imprimir la penultima nora
+print(notas[-2])
