c4565a3011ff2f5cb08e914fe890d08ec65ff7c0
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-28 00:39:32 +0000

    cccccccccccccccc

---
+++ 31_app.py
@@ -0,0 +1,18 @@
+# Listas
+
+# Lista de nombres de meses del año del primer trimestre
+meses = ["Enero", "Febrero", "Marzo"]
+
+# Imprimir lista de elementos
+print(meses);
+      
+# Agregar elementos de mes
+meses.append("Abril")
+
+# Imprimir la lista de elementos actualizado
+print(meses)
+
+# Agregar un elemento de número
+meses.append(123)
+
+print(meses)
