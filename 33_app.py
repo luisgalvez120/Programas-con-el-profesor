b6d07ea588eedb461f364ac8d905a9700b2eea6e
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-28 01:10:46 +0000

    Listas

---
+++ 33_app.py
@@ -0,0 +1,16 @@
+# Lista
+
+# Tenemos area de una organizacion
+areas = ["Administracion", "Recursos Humanos", "Contabilidad", "Sistemas"]
+
+# Buscar elementos dentro de la lista
+print("Recursos Humanos" in areas)
+
+# Buscar eleento dentro de lista
+print("Recursos Humanos" in areas)
+
+# Buscar contabilidad
+print("Contabilidad" in areas)
+
+# Buscar Sistemas
+print("Sistemas" in areas)
---
+++ 34_app.py
@@ -0,0 +1,11 @@
+# Lista
+
+# Tenemos area de una organizacion
+areas = ["Administracion", "Recursos Humanos", "Contabilidad", "Sistemas"]
+
+# Obtener posicion (indexados) del eleento de lista
+print(areas.index("Sistemas"))
+
+print(areas.index("Recursos Humanos"))
+
+print(areas.index("Contabilidad"))
