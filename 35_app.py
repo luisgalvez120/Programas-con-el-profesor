c8cacf4d6ae1228bcef5e6b74c7c10eaf44eec37
luisgalvez120 <i202518022@cibertec.edu.pe>
2025-10-28 01:36:58 +0000

    tupla

---
+++ 35_app.py
@@ -0,0 +1,13 @@
+# Tupla
+
+# Crear una tupla con diversos tipos de elementos
+tupla = (4, 20, "Ysabel", 15.75, ["Manzana", "Fresa"], 100)
+
+lista = [4, 20, "Ysabel", 15.75, ["Manzana", "Fresa"], 100]
+
+# Imprimir los elementos de la tupla
+print(tupla)
+
+# Imprimir tipo de elemento
+print(type(tupla))
+print(type(lista))
